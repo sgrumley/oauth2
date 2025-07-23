1c40ee5b740c201ae8d6a226627e1ac6221370dc
sgrumley <grumsamu@gmail.com>
2025-07-23 23:07:37 +1000

    refactor pkce

--- cmd/flows/pkce/main.go
+++ cmd/flows/pkce/main.go
@@ -13,12 +13,13 @@ import (
 	"github.com/sgrumley/oauth/pkg/config"
 	"github.com/sgrumley/oauth/pkg/logger"
 	"github.com/sgrumley/oauth/pkg/web"
+	"golang.org/x/oauth2"
 )
 
 var (
 	pkceChan  = make(chan string)
 	stateChan = make(chan string)
-	scopes    = "posts read"
+	// scopes    = "posts read"
 )
 
 type PKCEConfig struct {
@@ -69,31 +70,38 @@ func PKCEFlow(ctx context.Context, cfg *PKCEConfig) {
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	defer cancel()
 
-	cli := &http.Client{
-		Timeout: 3 * time.Minute,
-	}
-
 	state, err := auth.GenerateState()
 	if err != nil {
 		logger.Fatal(ctx, "failed to generate state", err)
 	}
 
-	client := authcode.NewClient(cfg.ClientID, cfg.RedirectURI, cfg.TokenURL, cfg.AuthURL, cli)
+	conf := &oauth2.Config{
+		ClientID: cfg.ClientID,
+		Endpoint: oauth2.Endpoint{
+			AuthURL:  cfg.AuthURL,
+			TokenURL: cfg.TokenURL,
+		},
+		RedirectURL: cfg.RedirectURI,
+		Scopes:      []string{"read post"},
+	}
 
 	// Step 1: Create a secret code verifier and code challenge
 	codeVerifier := auth.GenerateCodeVerifier()
 	codeChallenge := auth.GenerateCodeChallenge(codeVerifier)
-	client.SetPKCE(codeVerifier, codeChallenge, "S256")
 
 	// Step 2: Build the authorization URL and redirect the user to the auth server
-	// TODO: build out the auth code to accept PKCE
-	if err := client.GetAuthorizationCode(ctx, scopes, state); err != nil {
+	codeURL := conf.AuthCodeURL(state,
+		oauth2.SetAuthURLParam("code_challenge", codeChallenge),
+		oauth2.SetAuthURLParam("code_challenge_method", "S256"),
+	)
+
+	if err := authcode.GetAuthorizationCode(ctx, codeURL); err != nil {
 		logger.Error(ctx, "get auth code request failed", err)
 		return
 	}
 
 	logger.Info(ctx, "[Client] Waiting for authorization code")
-	// TODO: look for a better way to do this? A way to get the correct authcode if called multiple times?
+	// TODO: look for a better way to do this? A way to get the correct authcode if called multiple times? use clientID+state?challenge??
 	select {
 	case <-ctx.Done():
 		logger.Fatal(ctx, "deadline exceeded for callback", fmt.Errorf("server timeout"))
@@ -103,18 +111,22 @@ func PKCEFlow(ctx context.Context, cfg *PKCEConfig) {
 	returnedState := <-stateChan
 	logger.Info(ctx, "[Client] Auth Code Response", slog.String("code", authCode))
 
+	if state != returnedState {
+		logger.Fatal(ctx, "server error", fmt.Errorf("state mismatch: expected %s, got %s", state, returnedState))
+	}
+
 	// Step 4: Exchange the auth code and code verifier for an access token
 	logger.Info(ctx, "[Client] Calling /token")
-	token, err := client.ExchangeCodeForToken(ctx, authCode, returnedState, state)
+	token, err := conf.Exchange(ctx, authCode, oauth2.SetAuthURLParam("code_verifier", codeVerifier))
 	if err != nil {
 		logger.Fatal(ctx, "server error", err)
 	}
+
 	logger.Info(ctx, "[Client] flow completed with Token",
 		slog.String("access_token", token.AccessToken),
-		slog.String("refresh_token", token.RefreshToken),
-		slog.Int("expire_time", token.ExpiresIn),
+		slog.String("refresh_token", token.RefreshToken), // this will come in a later commit
+		slog.Int64("expire_time", token.ExpiresIn),
 		slog.String("token_type", token.TokenType),
-		slog.String("scope", token.Scope),
 	)
 
 	logger.Info(ctx, "Shutting down client")
